3ce768454f2f8c15c5ba7e216e626d39950a4f8f
jfrontel <114813145+jfrontel@users.noreply.github.com>
2022-10-02 18:06:11 +0200

    Add files via upload

---
+++ ft_isalnum.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfrontel <jfrontel@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 12:04:56 by jfrontel          #+#    #+#             */
+/*   Updated: 2022/04/29 13:52:49 by jfrontel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+int	ft_isalnum(int c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c))
+		return (1);
+	return (0);
+}
+
+/*int	main(void)
+{
+	printf("%d\n", ft_isalnum('\n'));
+}*/
